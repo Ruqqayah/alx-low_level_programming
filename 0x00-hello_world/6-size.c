bda8a3330a1ab51e6d828eceef1c098b37c15199
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-10 01:03:01 -0800

    Commit for size of various data types task six

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - C program that prints the size of various data types
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("size of char is %d bytes" a);
+	printf("size of int is %d bytes" b);
+	printf("size of long int is %d bytes" c);
+	printf("size of long long int is %d bytes" d);
+	printf("size of float is %d bytes" e);
+	return (0);
+}
