dc604acfee2f04d163ac3ea541a43a6e1cfc94c0
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-12 10:15:40 -0800

    Commit for 2-print_alphabet.c task two

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while
+		(alpha <= 'z');
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
