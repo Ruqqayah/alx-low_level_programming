1ab41605033b139ce866b14e667f16f92c2b7038
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-11 17:50:58 -0800

    Commit for C program to the standard errror task eight

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print a C program, followed by a new line and to the standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	putc(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
