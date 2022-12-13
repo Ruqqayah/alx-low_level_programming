959e2e2e86dcebc1c1571399854d2e889ec7bffc
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-12 16:15:42 -0800

    Commit for 5-print_numbers.c task five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while
+		(num <= 10)
+	{
+		printf("%d\n", num);
+		num++;
+	}
+
+	return (0);
+}
+
