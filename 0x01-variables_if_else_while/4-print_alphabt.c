6cfa262ec798733fb9ad3fe19c438a68a35dc4ff
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-12 16:03:10 -0800

    Commit for 4-print_alphabt.c task four

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase without two alphabeths.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha;
+	char alphe = 'e';
+	char alphq = 'q';
+
+	for (alpha = 'a'; alpha = 'z'; alpha++)
+
+	{
+		if (low != alphe && low != alphq)
+			putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
