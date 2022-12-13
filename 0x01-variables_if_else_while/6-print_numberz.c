c8eabc35bf18adaf6255ed8343a9a90a03f77928
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-12 16:23:32 -0800

    Commit for 6-print_numberz.c task six

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print alphabet in lowercase, then uppercase, and then new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while
+		(num = 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
