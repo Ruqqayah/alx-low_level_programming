1c810f6e00c4fe6ad9c429b384f4e74573bc0756
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-12 16:06:01 -0800

    Commit for 4-print_alphabt.c task four

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,7 +14,7 @@ int main(void)
 	char alphe = 'e';
 	char alphq = 'q';
 
-	for (alpha = 'a'; alpha = 'z'; alpha++)
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
 
 	{
 		if (alpha != alphe && alpha != alphq)
