07d30ab6026e1a03943c174b651b7a29eafa6662
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-14 22:47:52 -0800

    Commit for 104-fibonacci.c task sixteen

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Finds and prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	long int a, b, c, i;
+
+	a = 1;
+	b = 2;
+
+	printf("%i, %i, ", a, b);
+	for (i = 0; i <= 98; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+
+		printf("%lu", c);
+
+		if (c < 5035488507601418376)
+			printf(", ");
+	}
+}
