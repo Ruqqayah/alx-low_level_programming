3da38ed75587933c97a0dc76740c3560ec3de977
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-14 22:27:52 -0800

    Commit for 101-natural.c task thirteen

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of all the multiples
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5) == 0 || (i % 3) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
