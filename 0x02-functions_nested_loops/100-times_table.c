6aeb96e1630643eea6bea2b89f406063b3e7f5ff
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-14 23:31:36 -0800

    Commit for 100-times_table.c task twelve

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,57 +1,46 @@
-#include "main.h"
+#include "coding.h"
 
 /**
- * print_times_table - Prints times table for numbers from 0-14
- * @n: An input integer value
- * Return: 0
+ * print_times_table - function with int type single argument
+ * @n: int type single argument
  */
+
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, result;
 
-	if (n > 0 && n < 15)
+	if (n < 16 && n >= 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
 			for (j = 1; j <= n; j++)
-				_putchar(i * j);
+			{
+				result = i * j;
+				if (result > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result / 10 + '0');
+					_putchar(result % 10 + '0');
+				} else if (result < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result % 10 + '0');
+				}
+			}
 			_putchar('\n');
 		}
-	}
-}
-
-/**
- * putformat - Formatted characters to output
- * @n: number to format
- * Return: 0
- */
-
-void putformat(int n)
-{
-
-	if (n <= 9)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
 	}
 }
