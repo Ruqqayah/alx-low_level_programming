ce1ad49a35eb93cbfae0116e4072a543113e9b06
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-14 20:15:14 -0800

    Commit for 3-islower.c task three

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - A function that checks for lowercase character
+ * @c: An input character
+ * Return: 1 if is lowercase
+*/
+
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1
+	}
+
+	return (lower);
+}
