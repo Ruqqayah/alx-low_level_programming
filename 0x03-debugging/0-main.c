a08b240bb5b7a3260a2461fec1e2ed48d490dc97
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-15 18:45:16 -0800

    Commit for 0-main.c tast zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - Print if integer is positive or negative
+ * @i: An input interger
+ * Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
