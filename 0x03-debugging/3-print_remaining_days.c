09e2589910fb4970c45f9f9cba55631645aafe28
Ruqqayah <ogunleye.ruqayyah@gmail.com>
2022-12-15 19:17:18 -0800

    Commit for 3-print_remaining_days.c tast three

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,7 +7,7 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
- * Return: 0
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
@@ -34,5 +34,4 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-	return (0);
 }
